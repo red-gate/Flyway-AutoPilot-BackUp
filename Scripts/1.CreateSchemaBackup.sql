512fc3058e38e26fe61122a7341d4420139c153d
Chris Hawkins <94678272+csnhawkins@users.noreply.github.com>
2024-12-09 14:16:08 +0400

    Merge of latest YAML pipeline definitions and simplification of setup scripts (#6)
    * Updated GitHub and AzureDevOps with latest YAML pipeline definitions to function with Flyway V11
    * Updated script to include all variables at the top of the script for easier editing.
    * Updated structure of script to be more easily edited
    * Updated comments in script 3 for easier editing
    * Updated TOML Project ID to be different to FastTrack.

--- Scripts/1.CreateSchemaBackup.sql
+++ Scripts/1.CreateSchemaBackup.sql
@@ -1,12 +1,13 @@
+----------------------------------------------------- Changes Required In This Section (Look for the 2 steps outlined) --------------------------------------------------------
 -- Variables for source and cloned database names
-DECLARE @SourceDB NVARCHAR(128) = N'AutoPilotDev'; -- Step 1. Change me to the DB name you wish to use!
-DECLARE @BackupDB NVARCHAR(128) = @SourceDB + N'_Schema'; -- Define the cloned database name by appending '_Schema' to the source DB name
+DECLARE @SourceDB NVARCHAR(128) = N'AdventureWorks'; -- Step 1. Change me to the DB name you wish to use!
+DECLARE @BackupDB NVARCHAR(128) = @SourceDB + N'_Schema'; -- Optional - Define the cloned database name by appending '_Schema' to the source DB name
+-- Define the Flyway Project Backup location
+DECLARE @BackupPath NVARCHAR(256) = N'C:\git\AutoPilot\backups\AutoBackup_Customer.bak';  -- Step 2. Change me to match the location of Flyway Project
 
+----------------------------------------------------- DON'T CHANGE BELOW THIS LINE --------------------------------------------------------
 -- Clone the source database schema only
 DBCC CLONEDATABASE (@SourceDB, @BackupDB) WITH NO_STATISTICS, NO_QUERYSTORE, VERIFY_CLONEDB; -- Clone the source database schema without statistics and Query Store
-
--- Backup the cloned database
-DECLARE @BackupPath NVARCHAR(256) = N'C:\WorkingFolders\FWD\AutoRodent\backups\AutoBackup_Customer.bak';  -- Step 2. Change me to match the location of Flyway Project
 
 -- Construct the BACKUP DATABASE command
 DECLARE @BackupCommand NVARCHAR(MAX) = 
